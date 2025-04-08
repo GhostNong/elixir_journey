6419b4cb19de6721b5fd98964e55809a718fc163
GhostNong <gianlucasciberras@gmail.com>
2025-04-08 10:32:36 +0200

    Users route

--- forum/lib/forum_web/controllers/page_controller.ex
+++ forum/lib/forum_web/controllers/page_controller.ex
@@ -1,3 +1,6 @@
+# EEx template engine used for rendering HTML [Elixir]
+# EEx Docs: https://hexdocs.pm/eex/1.12.3/EEx.html
+
 defmodule ForumWeb.PageController do
   use ForumWeb, :controller
 
@@ -5,5 +8,16 @@ defmodule ForumWeb.PageController do
     # The home page is often custom made,
     # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  def users(conn, _params) do
+    # The users page is often custom made,
+    # so skip the default app layout.
+    users = [
+      %{id: 1, name: "Alice", email: "alice@gmail.com"},
+      %{id: 2, name: "John", email: "john@gmail.com"},
+      %{id: 3, name: "Alex", email: "alex@gmail.com"}
+    ]
+    render(conn, :users,users: users, layout: false)
   end
 end
